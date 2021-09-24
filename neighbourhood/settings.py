ec9272fd086dda2d715eca4b805d78922bb6bcce
Robert Maina <robert.maina@student.moringaschool.com>
2021-09-24 09:09:16 +0300

    configured postsql database

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -88,8 +88,10 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vicinia',
+        'USER': 'moringa',
+    'PASSWORD':'1234',
     }
 }
 
